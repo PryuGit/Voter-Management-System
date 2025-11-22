439363b7196ed7bbed6fddd1e9050802c15c7d9e
Pratyush <shakyapratyush.ps@gmail.com>
2025-11-22 19:14:13 +0545

    providing conditional output of voting result(ascending/descending) and formatted the user interface

--- voter.py
+++ voter.py
@@ -3,36 +3,58 @@ def  vote(n):#VOTING CANDIDATES
     c2=0
     c3=0
     dict={}
+    print("======================================")
+    print("Candidates")
+    print("( $ ) for Navin")
+    print("( & ) for Nick")
+    print("( * ) for Borat")
+    print("--------------------------------------")
     for x in range(n):
-        c=input("Choose candidate(C1/C2/C3):").upper()
-        if(c=="C1"):
+        c=input("Choose candidate:").upper()
+        if(c=="$"):
             c1=c1+1
-            dict.update({"C1":c1})
-        elif(c=="C2"):
+            dict.update({"Navin":c1})
+        elif(c=="&"):
             c2=c2+1
-            dict.update({"C2":c2})
-        elif(c=="C3"):
+            dict.update({"Nick":c2})
+        elif(c=="*"):
             c3=c3+1
-            dict.update({"C3":c3})
+            dict.update({"Borat":c3})
         else:
             print("Invalid Data")
     value={k:v for k,v in sorted(dict.items(), key=lambda item:item[1])}
     reverse={k:v for k,v in sorted(dict.items(), key=lambda item:item[1],reverse="true")}
-    print(value)
-    print(reverse)
-
+    
+    print("--------------------------------------")
+    ans=input("Descending or Ascending??(D/A):")
+    if ans.upper() == 'A':
+        print("Ascending\n",value)
+    elif ans.upper() == 'D':
+        print("Descending\n",reverse)
+    else:
+        while ans.upper()!='A' and ans.upper()!='D':
+            print("Error in the system.")
+            ans=input("Descending or Ascending??(D/A):")
+        if ans.upper() == 'A':
+            print("Ascending\n",value)
+        elif ans.upper() == 'D':
+            print("Descending\n",reverse)
+    print("======================================")         
+    
 def voter_count():#COUNTING VOTERS
-    print("=====================================")
+    print("======================================")
     print("Voting System")
-    print("=====================================")
+    print("======================================")
     n=int(input("Enter Number of Voters:"))
     while (n<=0):
         print("Invalid Voter Count!!")
         n=int(input("Enter Number of Voters:"))
     return n
 
-c=voter_count()
-vote(c)
+count=voter_count()
+voter=vote(count)
+
+
 
 
 
